7c7e563659506cfc58a825a0b1726faa51bbf63c
Derek Anderson <derek@armyofevilrobots.com>
2014-10-13 16:39:02 -0700

    Remove old ca.clj, fix lein project

--- src/life_seesaw/main.clj
+++ src/life_seesaw/main.clj
@@ -1,36 +1,39 @@
-(ns life-seesaw.ca)
+;(use 'clojure.repl)
+(ns life_seesaw.main (:gen-class))
 
 (import '(javax.swing JFrame JPanel)
         '(java.awt Color Graphics)
         '(java.awt.image BufferedImage))
- 
+;TODO: Replace all this with imports
+(use 'seesaw.core)
+(use 'seesaw.color)
+(use 'seesaw.graphics)
+
 (def dim-board   [ 90   90])
 (def dim-screen  [600  600])
 (def dim-scale   (vec (map / dim-screen dim-board)))
  
 (defn fmap [f coll] (doall (map f coll)))
- 
+
 (defn render-cell [#^Graphics g cell]
   (let [[state x y] cell
         x  (inc (* x (dim-scale 0)))
         y  (inc (* y (dim-scale 1)))]
     (doto g
-      (.setColor (if (= state :dying) Color/GRAY Color/WHITE))
+      (.setColor (if (= state :dying) (color "gray") (color "white")))
       (.fillRect x y (dec (dim-scale 0)) (dec (dim-scale 1))))))
- 
+
 (defn render [g img bg stage]
-  (.setColor bg Color/BLACK)
-  (.fillRect bg 0 0 (dim-screen 0) (dim-screen 1))
+  (doto bg
+    (.setColor (color "black"))
+    (.fillRect 0 0 (dim-screen 0) (dim-screen 1)))
   (fmap (fn [col]
           (fmap #(when (not= :off (% 0))
                    (render-cell bg %)) col)) stage)
-  (.drawImage g img 0 0 nil))
- 
-(def board
-     (for [x (range (dim-board 0))]
-       (for [y (range (dim-board 1))]
-         [(if (< 50 (rand-int 100)) :on :off) x y])))
- 
+  (.drawImage g img 0 0 nil)
+  )
+
+
 (defn active-neighbors [above [left _ right] below]
   (count
    (filter #(= :on (% 0))
@@ -46,25 +49,39 @@
       (= :dying self)                              [:off   x y]
       (= 2 (active-neighbors above current below)) [:on    x y]
       :else                                        [:off   x y])))
- 
+
+
 (defn step [board]
   (doall
    (pmap (fn [window]
           (apply #(doall (apply map rules %&))
                  (doall (map torus-window window))))
         (torus-window board))))
- 
+
+(def board
+     (for [x (range (dim-board 0))]
+       (for [y (range (dim-board 1))]
+         [(if (< 50 (rand-int 100)) :on :off) x y])))
+
 (defn activity-loop [surface stage]
   (while true
     (swap! stage step)
     (.repaint surface)))
- 
+
+
+
 (defn -main []
-(let [stage (atom board)
-      frame (JFrame.)
-      img   (BufferedImage. (dim-screen 0) (dim-screen 1) (BufferedImage/TYPE_INT_ARGB))
-      bg    (.getGraphics img)
-      panel (doto (proxy [JPanel] [] (paint [g] (render g img bg @stage))))]
-  (doto frame (.add panel) .pack (.setSize (dim-screen 0) (dim-screen 1)) .show
-        (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE))
-  (future (activity-loop panel stage))))
+  (let [stage (atom board)
+        img   (buffered-image (dim-screen 0) (dim-screen 1))
+        bg    (.createGraphics img)
+        panel (canvas :paint #(render %2 img bg @stage))
+        caframe (frame :title "SeeSawCA")
+        ]
+    (native!)
+    (doto caframe (config! :content panel)
+      (.setDefaultCloseOperation javax.swing.JFrame/EXIT_ON_CLOSE)
+      (pack!)
+      (.setSize (dim-screen 0) (dim-screen 1))
+      (show!) )
+    (future (activity-loop panel stage))
+    ))
